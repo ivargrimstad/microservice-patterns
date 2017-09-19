06d785085cd68322522899181c785aa10b76048b
Ivar Grimstad <ivar.grimstad@gmail.com>
2017-09-19 20:41:59 +0200

    Clean up

--- dukes-liberty/src/main/java/eu/agilejava/dukes/JaxrsApplication.java
+++
@@ -1,9 +0,0 @@
-package eu.agilejava.dukes;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class JaxrsApplication extends Application {
-
-}
