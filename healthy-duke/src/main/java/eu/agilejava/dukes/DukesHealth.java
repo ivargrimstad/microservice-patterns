9afac27793c7e3359698097aeda7fcb1dbe06aa3
Ivar Grimstad <ivar.grimstad@gmail.com>
2017-09-20 08:52:45 +0200

    Add MicroProfile Health Demo

---
+++ healthy-duke/src/main/java/eu/agilejava/dukes/DukesHealth.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.agilejava.dukes;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import org.eclipse.microprofile.health.Health;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+
+/**
+ *
+ * @author Ivar Grimstad (ivar.grimstad@gmail.com)
+ */
+@Health
+@Path("health")
+@ApplicationScoped
+public class DukesHealth implements HealthCheck {
+
+    @GET
+    @Produces(APPLICATION_JSON)
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("successful-check").up().build();
+    }
+}
