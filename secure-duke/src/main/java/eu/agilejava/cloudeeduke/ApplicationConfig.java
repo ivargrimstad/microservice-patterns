14f843325ba3407798b69dd697f97db6751b9e3f
Ivar Grimstad <ivar.grimstad@gmail.com>
2018-10-24 11:57:03 -0700

    Update security demo

--- secure-duke/src/main/java/eu/agilejava/cloudeeduke/ApplicationConfig.java
+++ secure-duke/src/main/java/eu/agilejava/cloudeeduke/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package eu.agilejava.cloudeeduke;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
@@ -7,6 +8,7 @@ import javax.ws.rs.core.Application;
  *
  * @author Ivar Grimstad (ivar.grimstad@cybercom.com)
  */
+@ApplicationScoped
 @ApplicationPath("")
 public class ApplicationConfig extends Application {
 }
