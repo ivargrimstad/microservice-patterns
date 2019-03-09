aa395f20ca0baf9c63597f30d0c4d3df5c7debc1
Ivar Grimstad <ivar.grimstad@gmail.com>
2019-03-09 08:02:52 -0500

    Add tests to packaged Duke

---
+++ packaged-duke/src/test/java/dukes/HelloResourceTest.java
@@ -0,0 +1,24 @@
+package dukes;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class HelloResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello, World!"));
+    }
+
+
+}
